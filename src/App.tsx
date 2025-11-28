e3cd9ca1cdda178b1389abd9fb530959eba69f01
parvez13913 <parvezz13913@gmail.com>
2025-11-28 08:46:35 +0600

    [DONE] Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router";
+import { ChatPage } from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+function App() {
+  return (
+    <div className="min-h-screen p-4 overflow-hidden relative flex items-center justify-center bg-slate-800">
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
+      <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
+
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
